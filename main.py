1d4d798cf52aaa4152a09521f73168149086900a
wilcer1 <wille.cervin@live.se>
2022-12-06 11:03:15 +0100

    ewqewq

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from pyspark.sql import SparkSession
+from datetime import datetime, date
+import pandas as pd
+from pyspark.sql import Row
 
 spark = SparkSession.builder.getOrCreate()
+
+#load dataframe from csv
+df = spark.read.csv('.csv', header=True, inferSchema=True)
+
 
