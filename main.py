6bfca3a55124ecda38360384a3b710322c99bb49
wilcer1 <wille.cervin@live.se>
2022-12-30 19:05:29 +0100

    twis alive

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import matplotlib.ticker as ticker
 
 spark = SparkSession.builder.appName('demo').master('local').enableHiveSupport().getOrCreate()
 
-df = spark.read.format("csv").option("header", "true").load("hdfs://localhost:9000/sample/dataset/1987.csv")
+
 
 
 
@@ -19,9 +19,11 @@ starting_point = 1987
 ending_point = 2009
 df = {}
 pandasDF = {}
-
+x = 0
 for i in range(starting_point,ending_point):
     df[str(i)] = spark.read.format("csv").option("header", "true").load("hdfs://localhost:9000/sample/dataset/"+str(i)+".csv")
+    print(x)
+    x+=1
     
 
 # convert all pyspark dataframes to pandas dataframes
@@ -80,28 +82,40 @@ def plot_null_value_of_file(file, name):
     plt.savefig(f"img\\NA_values_{name}.png")
     plt.clf()
 
+
+
 def plot_air_delay():
-    plt.figure(figsize=(10, 10))
-    max_delay = 0
+    plt.figure(figsize=(15, 10))
+    avg_delays = {}
     file_names = []
+    max_avg = 0
+    i = 0
     for name, file in df.items():
+        print("name: ", name, f"{i}/{len(df)}")
+        i+=1
         
         # get the list of delays
-        delays = file.select("ArrDelay").toPandas()["ArrDelay"].tolist()
+        delays = file.select("ArrDelay")
         # filter out the NA values
-        delays_filtered = [d for d in delays if d != 'NA']
-        # convert the list of strings to integers and find max delay
-        max_delay = max(max_delay, max(map(int, delays_filtered)))
-        plt.plot(delays, label=name)
-        
+        avg_delay = delays.agg(F.avg("ArrDelay"))
+        # get the average delay of the file
+        avg_delay = avg_delay.first()[0]
+        max_avg = max(max_avg, avg_delay)
+        avg_delays[name] = avg_delay
         file_names.append(name)
     
+    plt.plot(file_names, avg_delays.values())
     plt.xlabel("Year")
-    plt.ylabel("Air Delay (minutes)")
-    plt.title("Air Delay over Time")
-    plt.legend()
+    plt.ylabel("Arr Delay (minutes)")
+    plt.title("Arr Delay over Time")
+    # plt.legend()
     # set the y-axis ticks to be every 60 minutes and the max value to be the max delay
-    plt.yticks(range(0, max_delay+1, 60))
+    plt.ylim(0, round(max(avg_delays.values()))+1)
+
+    # Set the y-axis ticks
+    plt.yticks(range(0, round(max(avg_delays.values()))+1))
+    print(round(max_avg))
+    plt.xticks(range(0, len(file_names), 1))
 
     # Set the tick labels to the file names using a FixedFormatter
     formatter = ticker.FixedFormatter(file_names)
@@ -114,8 +128,10 @@ def plot_air_delay():
 
 
 
+
 def main():
     plot_air_delay()
+    
     
    
 
